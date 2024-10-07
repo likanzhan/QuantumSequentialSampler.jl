717c517f04146b58fe282677cd21c9c691cbd426
Likan Zhan <zhanlikan@hotmail.com>
2024-10-07 08:53:13 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.52

---
+++ src/QuantumSequentialSampler.jl
@@ -0,0 +1,5 @@
+module QuantumSequentialSampler
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using QuantumSequentialSampler
+using Test
+
+@testset "QuantumSequentialSampler.jl" begin
+    # Write your tests here.
+end
